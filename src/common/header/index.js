e0cdbc8529eab4868c8254a9803228e8d49aa9c3
nilkanthdhanani <nilkanth286.rejoice@gmail.com>
2024-11-29 18:37:02 +0530

    header add

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,13 +1,56 @@
+"use client"
+import { useState, useEffect } from 'react';
 import LogoIco from '@/assets/images/svg/logo';
 import styles from './header.module.scss';
+import Link from 'next/link';
+import ProfileIco from '@/assets/images/svg/profile';
 
 export default function Header() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} ${scrolled ? styles.scrolled : ''}`}>
       <div className={styles.container}>
         <div className={styles.headerDiv}>
-          <div className={styles.headerDivLogo}>
-            <LogoIco />
+          <Link href={"/"}>
+            <div className={styles.logo}>
+              <LogoIco />
+              <div className={styles.logoText}>
+                <span>WeDu</span>
+              </div>
+            </div>
+          </Link>
+          <div className={styles.headerList}>
+            <nav>
+              <Link href={"/"}><span>Home</span></Link>
+              <Link href={"/"}><span>Product</span></Link>
+              <Link href={"/"}><span>Product Plans</span></Link>
+              <Link href={"/"}><span>Contact</span></Link>
+              <Link href={"/"}><ProfileIco /> <span>Log In</span></Link>
+            </nav>
+            <Link href={"/"}>
+              <button type="button">Get Started</button>
+            </Link>
+          </div>
+          <div className={styles.menuIcon}>
+            <label>
+              <input type="checkbox" />
+              <span></span>
+              <span></span>
+              <span></span>
+            </label>
           </div>
         </div>
       </div>
